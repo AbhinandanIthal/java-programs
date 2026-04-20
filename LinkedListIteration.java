675bfce86e1ffaeeca3cc685d25e3461f8d63ac0
AbhinandanIthal <abhinandanithal@gmail.com>
2026-04-20 20:57:04 +0530

    Add files via upload

---
+++ LinkedListIteration.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class LinkedListIteration {
+    public static void main(String[] args) {
+        // 1. Create and populate a LinkedList
+        LinkedList<String> colors = new LinkedList<>();
+        colors.add("Red");    // Index 0
+        colors.add("Green");  // Index 1 (2nd position)
+        colors.add("Black");  // Index 2
+        colors.add("White");  // Index 3
+
+        // 2. Initialize ListIterator starting at the 2nd position (index 1)
+        // Note: Java lists use 0-based indexing
+        ListIterator<String> iterator = colors.listIterator(1);
+
+        // 3. Iterate and print the remaining elements
+        System.out.println("Elements from the 2nd position onwards:");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
